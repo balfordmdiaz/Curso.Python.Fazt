a31f947d259093e88a6d60e8c12426fb34155f43
balford <balfordm21@gmail.com>
2020-01-17 08:25:26 -0600

    Curso Basico

---
+++ Index.py
@@ -0,0 +1,37 @@
+print('hello')
+
+# List 
+
+[10, 20, 30, 40, 50]
+['Hola', 'Bye', 'Adios']
+[10, 'Hello', True, 5.1]
+
+#Tuples
+#Es una lista donde los datos no se pueden cambiar
+(10, 20, 30, 40, 50)
+
+#Dictionaries
+
+{
+    "Nombre":"Balford",
+    "Apellido":"Montenegro",
+    "Edad":21
+}
+
+
+myStr = 'Hello World'
+
+print(myStr.upper())    #Metodo para poner en mayusculas
+print(myStr.lower())    #Metodo para poner en minusculas
+print(myStr.swapcase()) #Metodo para poner las letras al contrario de como se escriben
+print(myStr.capitalize())#Metodo para poner la letra capital de la primera palabra del texto
+print(myStr.replace('Hello', 'Bye'))#Metodo para remplazar las palabras
+print(myStr.count('l'))#Metodo para contar cuantas veces esta escrito la letra 'l'
+
+
+#Pedir que el usuario ingrese algun dato
+
+age = input("Insert your age: ")
+print("age: " + age)
+
+
